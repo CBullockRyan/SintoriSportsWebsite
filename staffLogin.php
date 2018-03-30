e61e367b0c421bf099afd81d312e86b782fe9605
bullocas <cassidybullock@msn.com>
2018-03-30 07:02:24 -0700

    sticky form

--- staffLogin.php
+++ staffLogin.php
@@ -119,8 +119,8 @@ Description: staff login page
 
 	<h1>Staff Login</h1>
 	<form action="staffLogin.php" method="post">
-		<p>Staff ID: <input type="text" name="staffID" maxlength="20" /></p>
-		<p>Password: <input type="password" name="staffPass" maxlength="40" /></p>
+		<p>Staff ID: <input type="text" name="staffID" maxlength="20" value='<?php echo $staffID ?>' /></p>
+		<p>Password: <input type="password" name="staffPass" maxlength="40" value='<?php echo $staffPass ?>' /></p>
 		<p><input type="submit" name="submit" value="submit" /></p>
 </body>
 
