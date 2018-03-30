13b261ad9a6d828de5ff974be6122898c13824f1
bullocas <cassidybullock@msn.com>
2018-03-30 08:34:40 -0700

    basic homepage... needs more work

--- nav.inc.php
+++ nav.inc.php
@@ -10,6 +10,8 @@ Description: navigation bar to be included on
 <html>
 
 <head>
+	<!-- css style sheet -->
+	<link rel="stylesheet" type="text/css" href="style.css">
 	<!-- BootstrapCDN links-->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
