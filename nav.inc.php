4fdef3110a02855ce24e6adf4fed4be04b98b810
bullocas <cassidybullock@msn.com>
2018-03-31 02:31:38 -0700

    staff navbar

--- nav.inc.php
+++ nav.inc.php
@@ -19,6 +19,7 @@ Description: navigation bar to be included on
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 </head>
 
+
 <body>
 	<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
 		<span class="navbar-brand">
