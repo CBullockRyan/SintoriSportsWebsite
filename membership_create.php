20459ce7a8e75aacb476b8b5e6f2c887af7b0164
bullocas <cassidybullock@msn.com>
2018-04-04 06:08:06 -0700

    basic form and page

---
+++ membership_create.php
@@ -0,0 +1,40 @@
+<!--************************************************
+Author: Cassidy Bullock
+Date: April 4, 2018
+Description: Create membership page, works with
+             sql tables membership and membershipinfo
+             and payment (initial payment is required).
+             if single membership is selected it should
+             not present option to add more members,
+             if membership is family should have
+             maximum of 5 members.
+************************************************-->
+<?php
+	if(session_status() == PHP_SESSION_NONE){
+		session_start();
+	}
+?>
+
+<!doctype html>
+
+<html>
+
+<head>
+	<title>New Membership</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<?php include ('staffnav.inc.php'); ?>
+	
+	<!-- form to create membership -->
+	<form action="membership_create.php" method="post">
+		<p>Membership Type: <select name="type">
+			<option value="1">Single Membership</option>
+			<option value="2">Family Membership</option>
+			<option value="3">Couple Membership</option>
+		</select></p>
+		<p><input type="submit" name="Submit" value="submit" /></p>
+</body>
+
+</html>
