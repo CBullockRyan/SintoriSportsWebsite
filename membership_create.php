ba1e43cdb7044640d4ea6a90dee2317c3d11234c
bullocas <cassidybullock@msn.com>
2018-04-06 11:15:58 +0100

    first part of php done... needs to be completed before it will work

--- membership_create.php
+++ membership_create.php
@@ -3,11 +3,7 @@ Author: Cassidy Bullock
 Date: April 4, 2018
 Description: Create membership page, works with
              sql tables membership and membershipinfo
-             and payment (initial payment is required).
-             if single membership is selected it should
-             not present option to add more members,
-             if membership is family should have
-             maximum of 5 members.
+             and payment (initial payment is required)
 ************************************************-->
 <?php
 	if(session_status() == PHP_SESSION_NONE){
@@ -27,10 +23,103 @@ Description: Create membership page, works with
 <body>
 	<?php include ('staffnav.inc.php'); ?>
 
+	<?php
+		//Create vars for form data
+		$mType="";
+		$fname="";
+		$lname="";
+		$gender="";
+		$phone="";
+		$address="";
+		$payment="";
+		$Dob="";
+		$email="";
+
+		//check form submission
+		if ($_SERVER['REQUEST_METHOD'] == 'POST'){
+			$errors = array();
+
+			//check all fields are filled out
+			if(empty($_POST['fname'])){
+				array_push($errors, "Please enter your first name <br/>");
+			}
+			else{
+				$fname=trim($_POST['fname']);
+			}
+			if(empty($_POST['lname'])){
+				array_push($errors, "Please enter your last name <br/>");
+			}
+			else{
+				$lname=trim($_POST['lname']);
+			}
+			if (empty($_POST['gender'])){
+				array_push($errors, "Please select your gender <br/>");
+			}
+			else{
+				$gender = $_POST['gender'];
+			}
+
+			//check hire date
+			if (empty($_POST['hire_date'])){
+				array_push($errors, "Please enter your hire date <br/>");
+			}
+			else{
+				$hired = $_POST['hire_date'];
+			}
+
+			//check for email
+			if(empty($_POST['emp_email'])){
+				array_push($errors, "Please enter valid email address <br/>");
+			}
+			else{
+				$email=trim($_POST['emp_email']);
+			}
+
+			if(empty($errors)){
+				//connect to database
+				require ('connect.php');
+
+				//fill variables
+				$empName = mysqli_real_escape_string($dbc, trim($empName));
+				$gender = mysqli_real_escape_string($dbc, trim($gender));
+				$hired = mysqli_real_escape_string($dbc, trim($hired));
+				$email = mysqli_real_escape_string($dbc, trim($email));
+
+				//insert data into database
+				$query = "Insert into employees (emp_name, gender, hire_date, emp_email) values ('$empName', '$gender', '$hired', '$email')";
+				$r = @mysqli_query($dbc, $query); //run query
+				//responses if it ran or didn't run correctly
+				if($r){
+					echo '<h1>Thank you for registering!</h1>';
+				}
+				else{
+					echo '<h1>System Error</h1>
+					<p class="error">You could not be registered due to system error. We apologize for any inconvenience.</p>';
+
+					echo '<p>' . mysqli_error($dbc) . '<br/><br/>Query: ' . $query . '</p>';
+				}
+
+				//disconnect from database
+				mysqli_close($dbc);
+
+				exit();
+			}
+			else{
+				echo "<h1>Errors</h1>";
+				echo "<p>The following errors occurred:<br/>";
+				foreach($errors as $error){
+					echo " - $error <br/>";
+				}
+				echo "Please try again</br>";
+			}
+		}
+	?>
+
+
 	<h1>Add Membership</h1>
 	<!-- form to create membership -->
 	<form action="membership_create.php" method="post">
-		<p>Membership Type: <select name="type">
+		<p>Membership Type: <select name="mType">
 			<option value="1">Single Membership</option>
 			<option value="2">Family Membership</option>
 			<option value="3">Couple Membership</option>
