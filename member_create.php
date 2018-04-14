439758eec1e264d3bfa77b73dc03d9e038e7857b
bullocas <cassidybullock@msn.com>
2018-04-14 09:47:30 +0100

    created

---
+++ member_create.php
@@ -0,0 +1,31 @@
+<!--************************************************
+Author: Cassidy Bullock
+Date: April 14, 2018
+Description: Update a staff member
+************************************************-->
+<?php
+	if(session_status() == PHP_SESSION_NONE){
+		session_start();
+	}
+?>
+
+<!doctype html>
+
+<html>
+
+<head>
+	<title>Add New Member</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+
+<body>
+	<?php include ('staffnav.inc.php'); ?>
+
+  <h1>Add a New Member: NOT WRITTEN YET</h1>
+  <p>This page hasn't been written yet. This should offer as many slots to enter
+  members as each membership type allows. This page can only be accessed by creating
+  a new membership.</p>
+
+</body>
+
+</html>
