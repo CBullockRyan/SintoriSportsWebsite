6cc09fa22d988689ebb7de4b7df808e4c5ebe444
bullocas <cassidybullock@msn.com>
2018-04-21 12:38:21 +0100

    move image upload from news to event

--- news_create.php
+++ news_create.php
@@ -26,18 +26,11 @@ Description: Create a news post
 		$title="";
 		$desc="";
     $date=date('Y-m-d H:i:s');
-		$imgPath="";
 
 		//check form submission
 		if ($_SERVER['REQUEST_METHOD'] == 'POST'){
 			//array to hold all errors
 			$errors = array();
-
-			//vars to upload image
-			$target_dir = "uploads/";
-			$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-			$uploadOk = 1;
-			$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
 			//check all fields are filled out
 			if(empty($_POST['title'])){
@@ -53,44 +46,6 @@ Description: Create a news post
 				$desc=trim($_POST['desc']);
 			}
 
-			//check image type
-			$check = getimagesize($_FILES["fileToUpload"]["tmp_name"]);
-    	if($check !== false) {
-        $uploadOk = 1;
-    	} else {
-        array_push($errors, "File is not an image.");
-        $uploadOk = 0;
-    	}
-
-			// Check if file already exists
-			if (file_exists($target_file)) {
-    		array_push($errors, "File already exists.");
-    		$uploadOk = 0;
-			}
-
-			// Check file size
-			if ($_FILES["fileToUpload"]["size"] > 500000) {
-    		array_push($errors, "Image must be smaller than 500000.");
-    		$uploadOk = 0;
-			}
-
-			// Allow certain file formats
-			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-			&& $imageFileType != "gif" ) {
-    		array_push($errors, "Please make sure your file is an image. Only JPG, JPEG, PNG & GIF files are allowed.");
-    		$uploadOk = 0;
-			}
-
-			// Upload image if there are no errors
-			if ($uploadOk !== 0) {
-    		if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
-        	//set image path so that it can be displayed on other pages
-					$imgPath = $target_file;
-    		} else {
-        	array_push($errors, "File could not be uploaded due to system error.");
-    		}
-			}
-
 			//if there are no errors from the form or from uploading the image
 			if(empty($errors)){
 				//connect to database
@@ -99,10 +54,9 @@ Description: Create a news post
 				//fill variables
 				$title = mysqli_real_escape_string($dbc, trim($title));
         $desc = mysqli_real_escape_string($dbc, trim($desc));
-				$imgPath = mysqli_real_escape_string($dbc, trim($imgPath));
 
 				//insert data into membership table
-				$q1 = "Insert into news (title, description, newsDate, image) values ('$title', '$desc', '$date', '$imgPath')";
+				$q1 = "Insert into news (title, description, newsDate) values ('$title', '$desc', '$date')";
 				$r1 = @mysqli_query($dbc, $q1); //run query
 
 				//check query ran
@@ -135,11 +89,10 @@ Description: Create a news post
 
 	<h1>Add News Post</h1>
 	<!-- form to create news post -->
-	<form action="news_create.php" method="post" id="news" enctype="multipart/form-data">
+	<form action="news_create.php" method="post" id="news">
 		<p>Title of Post: <input type="text" name="title" value="<?php echo $title ?>" /></p>
 		<p>Description: </p>
     <p><textarea name="desc" form="news" rows="4" cols="40" value="<?php echo $desc ?>"></textarea></p>
-		<p>Image: <input type="file" name="fileToUpload" id="fileToUpload" /></p>
 		<p><input type="submit" name="Submit" value="submit" /></p>
 </body>
 
