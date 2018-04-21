de877e402df76bd3c844b4f05db2b04a383f34e6
bullocas <cassidybullock@msn.com>
2018-04-21 10:49:58 +0100

    image upload supported

--- news_create.php
+++ news_create.php
@@ -22,12 +22,18 @@ Description: Create a new event
 	<?php include ('staffnav.inc.php'); ?>
 
 	<?php
-		//Create vars for form data
+		//Create vars for news table
 		$title="";
 		$desc="";
     $date=date('d-m-Y');
     $time=date('H:i:sa');
-		$img="";
+		$imgPath="";
+
+		//vars to upload image
+		$target_dir = "uploads/";
+		$target_file = $target_dir . basename($_FILES["img"]["name"]);
+		$uploadOk = 1;
+		$imageFileType = pathinfo($target_file, PATHINFO_EXTENSION);
 
 		//check form submission
 		if ($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -35,25 +41,57 @@ Description: Create a new event
 
 			//check all fields are filled out
 			if(empty($_POST['title'])){
-				array_push($errors, "Please enter the event title. <br/>");
+				array_push($errors, "Please enter the event title.");
 			}
 			else{
 				$title=trim($_POST['title']);
 			}
 			if(empty($_POST['desc'])){
-				array_push($errors, "Please enter a description of the event. <br/>");
+				array_push($errors, "Please enter a description of the event.");
 			}
 			else{
 				$desc=trim($_POST['desc']);
 			}
-      if (empty($_POST['img'])){
-        array_push($errors, "Please add an image. <br/>");
-      }
-      else{
-        $img = $_POST['img'];
-      }
 
+			//check image type
+			$check = getimagesize($_FILES["img"]["tmp_name"]);
+    	if($check !== false) {
+        $uploadOk = 1;
+    	} else {
+        array_push($errors, "File is not an image.");
+        $uploadOk = 0;
+    	}
 
+			// Check if file already exists
+			if (file_exists($target_file)) {
+    		array_push($errors, "File already exists.");
+    		$uploadOk = 0;
+			}
+
+			// Check file size
+			if ($_FILES["img"]["size"] > 500000) {
+    		array_push($errors, "Image must be smaller than 500000.");
+    		$uploadOk = 0;
+			}
+
+			// Allow certain file formats
+			if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+			&& $imageFileType != "gif" ) {
+    		array_push($errors, "Please make sure your file is an image. Only JPG, JPEG, PNG & GIF files are allowed.");
+    		$uploadOk = 0;
+			}
+
+			// Upload image if there are no errors
+			if ($uploadOk !== 0) {
+    		if (move_uploaded_file($_FILES["img"]["tmp_name"], $target_file)) {
+        	//set image path so that it can be displayed on other pages
+					$imgPath = $target_file;
+    		} else {
+        	array_push($errors, "File could not be uploaded due to system error.");
+    		}
+			}
+
+			//if there are no errors from the form or from uploading the image
 			if(empty($errors)){
 				//connect to database
 				require ('connectDB.php');
@@ -61,10 +99,10 @@ Description: Create a new event
 				//fill variables
 				$title = mysqli_real_escape_string($dbc, trim($title));
         $desc = mysqli_real_escape_string($dbc, trim($desc));
-				$img = mysqli_real_escape_string($dbc, trim($img));
+				$imgPath = mysqli_real_escape_string($dbc, trim($imgPath));
 
 				//insert data into membership table
-				$q1 = "Insert into news (title, description, newsTime, newsDate, image) values ('$title', '$desc', '$time', '$date', '$img')";
+				$q1 = "Insert into news (title, description, newsTime, newsDate, image) values ('$title', '$desc', '$time', '$date', '$imgPath')";
 				$r1 = @mysqli_query($dbc, $q1); //run query
 
 				//check query ran
@@ -75,7 +113,7 @@ Description: Create a new event
 					echo '<h1>System Error</h1>
 					<p class="error">You could not be registered due to system error. We apologize for any inconvenience.</p>';
 
-					//echo '<p>' . mysqli_error($dbc) . '<br/><br/>Query: ' . $q1 . '</p>';
+					echo '<p>' . mysqli_error($dbc) . '<br/><br/>Query: ' . $q1 . '</p>';//debugging message
 				}
 
 				//disconnect from database
@@ -101,7 +139,7 @@ Description: Create a new event
 		<p>Title of Post: <input type="text" name="title" value="<?php echo $title ?>" /></p>
 		<p>Description: </p>
     <p><textarea name="desc" form="news" rows="4" cols="40" value="<?php echo $desc ?>"></textarea></p>
-		<p>Image: <input type="number" name="max" value="<?php echo $max ?>" /></p>
+		<p>Image: <input type="file" name="img" id="img" /></p>
 		<p><input type="submit" name="Submit" value="submit" /></p>
 </body>
 
