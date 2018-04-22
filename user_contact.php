715124c17b4cf84e85a99a4990532c6cf50fca39
bullocas <cassidybullock@msn.com>
2018-04-22 08:48:52 +0100

    partially completed sql portion

--- user_contact.php
+++ user_contact.php
@@ -19,20 +19,51 @@ Description: View contact information and make enquiry
 </head>
 
 <body>
-	<?php include ('nav.inc.php'); ?>
+	<?php include ('nav.inc.php');
 
-  <h1>Contact Us</h1>
-  <div>
-    <h2>Opening Hours</h2>
-    <p>Monday-Thursday 7:00am - 10:00pm</p>
-		<p>Friday-Saturday 9:00am - 8:00pm</p>
-  </div>
-	<div>
-		<h2>Contact Details</h2>
-		<p><b>Phone: </b>+1(503)555-7965</p>
-		<p><b>Address: </b>22 Flowerly Ln, Salem, OR 97302</p>
-		<p><b>Email: </b>enquiry@sintori.com</p>
-	</div>
+	//vars for enquiry form
+	$fname="";
+	$lname="";
+	$phone="";
+	$email="";
+	$subject="";
+	$message="";
+
+	// connect to database
+	require ('connectDB.php');
+
+	//query to get all information for location
+	$q = "SELECT * FROM location";
+	$r = @mysqli_query($dbc, $q);
+	$row=mysqli_fetch_array($r);
+
+	//make contact vars
+	$mt_open=$row['mon_thurs_open'];
+	$mt_close=$row['mon_thurs_close'];
+	$fs_open=$row['fri_sat_open'];
+	$fs_close=$row['fri_sat_close'];
+	$Lphone=$row['phone'];
+	$Lemail=$row['email'];
+	$Laddress=$row['address'];
+
+	//display top part of page with contact Details
+	echo "<h1>Contact Us</h1>";
+	echo "<h2>Opening Hours</h2>";
+	echo "<p>Monday-Thursday $mt_open - $mt_close</p>";
+	echo "<p>Friday-Saturday $fs_open - $fs_close</p>";
+	echo "<h2>Contact Details</h2>";
+	echo "<p><b>Phone: </b>$Lphone</p>
+				<p><b>Address: </b>$Laddress</p>
+				<p><b>Email: </b>$Lemail</p";
+
+	//if enquiry posted
+	if($_SERVER['REQUEST_METHOD'] == 'POST'){
+
+	} else{//print the form
+		echo "<h2>Get in Touch</h2>";
+		echo "<"
+	}
+?>
 </body>
 
 </html>
