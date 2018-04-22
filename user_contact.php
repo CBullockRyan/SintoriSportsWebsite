741623d177c6331615ae0d3f39ccb63debf87383
bullocas <cassidybullock@msn.com>
2018-04-22 09:33:45 +0100

    close tag

--- user_contact.php
+++ user_contact.php
@@ -54,8 +54,7 @@ Description: View contact information and make enquiry
 	echo "<h2>Contact Details</h2>";
 	echo "<p><b>Phone: </b>$Lphone</p>
 				<p><b>Address: </b>$Laddress</p>
-				<p><b>Email: </b>$Lemail</p";
-	echo "<h2>Get in Touch</h2>";
+				<p><b>Email: </b>$Lemail</p>";
 
 	//if enquiry posted
 	if($_SERVER['REQUEST_METHOD'] == 'POST'){
@@ -140,6 +139,7 @@ Description: View contact information and make enquiry
 			}
 		}
 	} else{//print the form
+		echo "<h2>Get in Touch</h2>";
 		echo "<form action='user_contact.php' method='post' id='enquiry'>
 		<p>First Name: <input type='text' name='fname' value='$fname' /></p>
 		<p>Last Name: <input type='text' name='lname' value='$lname' /></p>
