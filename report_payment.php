e0382c0f40945616181ca7f361d52e419bccc3fe
bullocas <cassidybullock@msn.com>
2018-04-23 12:19:06 +0100

    add jquery

--- report_payment.php
+++ report_payment.php
@@ -17,7 +17,12 @@ Description: payment report for user generated date
 <head>
 	<title>Payment Report</title>
 	<link rel="stylesheet" type="text/css" href="style.css">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js"></script>
+	<script
+  src="https://code.jquery.com/jquery-3.3.1.min.js"
+  integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+  crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.7.2/Chart.min.js">
+	</script>
 	<script type="text/javascript" src="linegraph.js"></script>
 </head>
 
