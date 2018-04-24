405811736ba27d3b628e35fc5b787c384f3c0042
bullocas <cassidybullock@msn.com>
2018-04-24 18:37:35 +0100

    switch them

--- data.php
+++ data.php
@@ -7,59 +7,39 @@ Description: enquiry status report
 	if(session_status() == PHP_SESSION_NONE){
 		session_start();
 	}
-
-  //connect to database
-  require ('connectDB.php');
-
-  //query to get information
-  $q = "SELECT resolved, count(*) AS numGroup GROUP BY resolved";
-  $r = @mysqli_query($dbc, $q);
 ?>
 
 <!doctype html>
 
 <html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
 
-<head>
-	<title>Enquiry Status Report</title>
-	<link rel="stylesheet" type="text/css" href="style.css">
-	<script type="text/javascript" src="js/jquery-3.3.1.js"></script>
-	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script type="text/javascript">
-    google.charts.load('current', {'packages':['corechart']});
-    google.charts.setOnLoadCallback(drawChart);
+      function drawChart() {
 
-    function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Task', 'Hours per Day'],
+          ['Work',     11],
+          ['Eat',      2],
+          ['Commute',  2],
+          ['Watch TV', 2],
+          ['Sleep',    7]
+        ]);
 
-      var data = google.visualization.arrayToDataTable([
-        ['Resolved', 'Unresolved'],
-        <?php
-          if(mysqli_num_rows($r) > 0){
-            while($row = mysqli_fetch_array($r)){
-              echo "['".$row['resolved']."', ".$row['numGroup']."],";
-            }
-          }
-        ?>
-      ]);
+        var options = {
+          title: 'My Daily Activities'
+        };
 
-      var options = {
-        title: 'Percentage of Resolved Enquiries',
-        width: 900,
-        height: 500,
-      };
+        var chart = new google.visualization.PieChart(document.getElementById('piechart'));
 
-      var chart = new google.visualization.PieChart(document.getElementById('piechart'));
-
-      chart.draw(data, options);
-}
-</script>
-</head>
-
-<body>
-	<?php include ('nav_staff.inc.php'); ?>
-
-
-	<div id="piechart"></div>
-</body>
-
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="piechart" style="width: 900px; height: 500px;"></div>
+  </body>
 </html>
