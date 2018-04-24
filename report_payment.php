918e43b180d5a9927218601a813b00da14b181cd
bullocas <cassidybullock@msn.com>
2018-04-24 19:27:43 +0100

    12 bars

--- report_payment.php
+++ report_payment.php
@@ -108,6 +108,12 @@ Description: payment report
           ["King's pawn (e4)", 44],
           ["Queen's pawn (d4)", 31],
           ["Knight to King 3 (Nf3)", 12],
+					["Knight to King 3 (Nf3)", 12],
+					["Knight to King 3 (Nf3)", 12],
+					["Knight to King 3 (Nf3)", 12],
+					["Knight to King 3 (Nf3)", 12],
+					["Knight to King 3 (Nf3)", 12],
+					["Knight to King 3 (Nf3)", 12],
           ["Queen's bishop pawn (c4)", 10],
           ['Other', 3]
         ]);
