9ae98286d9be07571159385b076774f08e98c3a3
bullocas <cassidybullock@msn.com>
2018-04-24 19:33:56 +0100

    change jan

--- report_payment.php
+++ report_payment.php
@@ -17,7 +17,7 @@ Description: payment report
 	WHERE datePaid BETWEEN '2018-01-01' AND '2018-01-31'";
 	$r = @mysqli_query($dbc, $q);
 	$row = mysqli_fetch_array($r);
-	$jan = $row[0];
+	$jan = 3;
 	//february
 	$q = "SELECT sum(amount) FROM payment
 	WHERE datePaid BETWEEN '2018-02-01' AND '2018-02-28'";
