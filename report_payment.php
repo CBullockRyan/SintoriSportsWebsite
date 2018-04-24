a0636b3fe8c0376fb6d05af22929d2e34c8085dd
bullocas <cassidybullock@msn.com>
2018-04-24 19:31:48 +0100

    remove extra tags

--- report_payment.php
+++ report_payment.php
@@ -95,10 +95,6 @@ Description: payment report
 	<link rel="stylesheet" type="text/css" href="style.css">
 	<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 	<script type="text/javascript">
-	<html>
-  <head>
-    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-    <script type="text/javascript">
       google.charts.load('current', {'packages':['bar']});
       google.charts.setOnLoadCallback(drawStuff);
 
