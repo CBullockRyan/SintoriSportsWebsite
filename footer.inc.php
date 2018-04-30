42021790d40837b6f0ef9abc82ea53846d40cd68
bullocas <cassidybullock@msn.com>
2018-04-30 11:46:46 +0100

    remove connection

--- footer.inc.php
+++ footer.inc.php
@@ -5,14 +5,12 @@ Description: footer inclusion
 ************************************************-->
 <?php
   //get contact information
-  require ('connectDB.php');
   $q = "SELECT phone, address, email FROM location";
   $r = @mysqli_query($dbc, $q);
   $row = mysqli_fetch_array($r);
   $phone = $row[0];
   $address = $row[1];
   $email = $row[2];
-  mysqli_close($dbc);
 ?>
 
 <footer class="footer">
