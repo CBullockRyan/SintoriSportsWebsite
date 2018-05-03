d8180ff13bed1eb36729503a48ab644c53647b81
bullocas <cassidybullock@msn.com>
2018-05-03 13:07:27 +0100

    try color

--- nav_staff.inc.php
+++ nav_staff.inc.php
@@ -22,15 +22,13 @@ Description: navigation bar to be included on
 
 
 <body>
-	<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-dark">
-		<span class="navbar-brand">
-			Sintori Sports Club
-		</span>
-		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
-		</button>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+	  <a class="navbar-brand" href="#">Sintori Sports Club</a>
+	  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation" style="">
+	    <span class="navbar-toggler-icon"></span>
+	  </button>
 
-		<div class="collapse navbar-collapse" id="navbarColor01">
+	  <div class="collapse navbar-collapse" id="navbarColor01">
 			<ul class="navbar-nav mr-auto">
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
